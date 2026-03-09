33ddbfbe94ef73f06dd30ef927f8f3e63f983007
NoFiix <khuddan@gmail.com>
2026-03-09 08:31:44 +0000

    refactor: TOKEN_TRACKER + TOKEN_ANALYST → GLOBAL_ prefix, architecture agent/skill clarifiée

--- skills_custom/trading/GLOBAL_TOKEN_ANALYST/index.js
+++ skills_custom/trading/GLOBAL_TOKEN_ANALYST/index.js
@@ -1,5 +1,5 @@
 import { runAgent } from "../_shared/agentRuntime.js";
 import { handler  } from "./handler.js";
 
-runAgent({ agentId: "TOKEN_ANALYST", handler, argv: process.argv })
+runAgent({ agentId: "GLOBAL_TOKEN_ANALYST", handler, argv: process.argv })
   .catch(e => { console.error("[TOKEN_ANALYST] fatal", e); process.exit(1); });
--- skills_custom/trading/GLOBAL_TOKEN_TRACKER/index.js
+++ skills_custom/trading/GLOBAL_TOKEN_TRACKER/index.js
@@ -1,5 +1,5 @@
 import { runAgent } from "../_shared/agentRuntime.js";
 import { handler  } from "./handler.js";
 
-runAgent({ agentId: "TOKEN_TRACKER", handler, argv: process.argv })
+runAgent({ agentId: "GLOBAL_TOKEN_TRACKER", handler, argv: process.argv })
   .catch(e => { console.error("[TOKEN_TRACKER] fatal", e); process.exit(1); });
